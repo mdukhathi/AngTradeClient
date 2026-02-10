544554a95cb81aeae56d90c648a12e24e0064e8c
Mdukhathi <mduduzi@terra.group>
2026-02-10 11:59:09 +0200

    Mock MsalService in tests to fix NullInjectorError

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MsalService } from '@azure/msal-angular';
+
+class MockMsalService {}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
+      providers: [
+        { provide: MsalService, useClass: MockMsalService }
+      ]
     }).compileComponents();
   });
 
