1a0fef251f1ae40db03a1b513d98b3e8b4d7e6b6
Mdukhathi <mduduzi@terra.group>
2026-02-10 12:15:00 +0200

    Stub MsalBroadcastService observables in tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,9 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { MsalService, MsalBroadcastService } from '@azure/msal-angular';
+import { of } from 'rxjs';
 
 class MockMsalService { instance = { getAllAccounts: () => [] }; }
-class MockMsalBroadcastService {}
+class MockMsalBroadcastService { inProgress$ = of(null); msalSubject$ = of(null); }
 
 describe('AppComponent', () => {
   beforeEach(async () => {
