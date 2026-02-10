5efb39f3646a8d4e1fa8c8418e6320df07c0dd1b
Mdukhathi <mduduzi@terra.group>
2026-02-10 12:28:50 +0200

    Align AppComponent template and test for title rendering

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -31,10 +31,18 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('AngTradeClient');
   });
 
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain('AngTradeClient');
+  // });
+
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('AngTradeClient');
-  });
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement as HTMLElement;
+  expect(compiled.querySelector('span')?.textContent).toContain('AngTradeClient app is running!');
+});
+
 });
